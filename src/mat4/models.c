128af50ce35903cf61eb0352b4214d4f3c3b673b
Clement RENAULT <crenault@e2r1p6.42.fr>
2015-05-13 18:13:10 +0200

    models updated

--- src/mat4/models.c
+++ src/mat4/models.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/10 23:06:44 by crenault          #+#    #+#             */
-/*   Updated: 2015/05/13 00:23:59 by crenault         ###   ########.fr       */
+/*   Updated: 2015/05/13 18:12:57 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,30 +25,25 @@ void				mat4_ident(t_mat4 *out)
 
 void				mat4_trans(t_mat4 *out, float x, float y, float z)
 {
-	ft_bzero(out->m, 4 * 4 * sizeof(float));
-	out->m[0] = 1.f;
-	out->m[5] = 1.f;
-	out->m[10] = 1.f;
+	mat4_ident(out);
 	out->m[12] = x;
 	out->m[13] = y;
 	out->m[14] = z;
-	out->m[15] = 1.f;
 }
 
 void				mat4_scale(t_mat4 *out, float x, float y, float z)
 {
-	ft_bzero(out->m, 4 * 4 * sizeof(float));
+	mat4_ident(out);
 	out->m[0] = x;
 	out->m[5] = y;
 	out->m[10] = z;
-	out->m[15] = 1.f;
 }
 void				mat4_rotate(t_mat4 *out, float ang, t_vec3 vec)
 {
 	t_vec3			unit;
 
 	unit = vec3_normal(vec);
-	ft_bzero(out->m, 4 * 4 * sizeof(float));
+	mat4_ident(out);
 	out->m[0] = cos(ang) + (unit.x * unit.x) * (1 - cos(ang));
 	out->m[1] = unit.x * unit.y * (1 - cos(ang)) - unit.z * sin(ang);
 	out->m[2] = unit.x * unit.z * (1 - cos(ang)) + unit.y * sin(ang);
@@ -58,5 +53,4 @@ void				mat4_rotate(t_mat4 *out, float ang, t_vec3 vec)
 	out->m[8] = unit.z * unit.x * (1 - cos(ang)) - unit.y * sin(ang);
 	out->m[9] = unit.z * unit.y * (1 - cos(ang)) + unit.x * sin(ang);
 	out->m[10] = cos(ang) + (unit.z * unit.z) * (1 - cos(ang));
-	out->m[15] = 1.f;
 }
