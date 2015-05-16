d79a3ee0330a858502449d0e0c7b33cbc9fbfdab
Clement RENAULT <crenault@e2r1p6.42.fr>
2015-05-16 18:39:54 +0200

    len is unsigned int now

--- src/vec3/simple.c
+++ src/vec3/simple.c
@@ -6,7 +6,7 @@
 /*   By: crenault <crenault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/12 12:29:58 by crenault          #+#    #+#             */
-/*   Updated: 2015/05/13 13:26:07 by crenault         ###   ########.fr       */
+/*   Updated: 2015/05/16 18:39:40 by crenault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,16 +25,13 @@ t_vec3				vec3(float x, float y, float z)
 
 t_vec3				vec3_normal(t_vec3 vec)
 {
-	float			len;
+	unsigned int	len;
 
 	len = vec3_len(vec);
 	return (vec3(vec.x / len, vec.y / len, vec.z / len));
 }
 
-float				vec3_len(t_vec3 vec)
+unsigned int		vec3_len(t_vec3 vec)
 {
-	float			len;
-
-	len = sqrt((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
-	return (len);
+	return (sqrt((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z)));
 }
